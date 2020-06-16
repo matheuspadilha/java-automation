90f79e8762255959ba4adeac1c7c40d5da472b62
Matheus Padilha <mvpadilha94@gmail.com>
2020-06-15 22:31:51 -0300

    add data and time screenshot

--- src/main/java/br/rs/matheuspadilha/core/BaseTest.java
+++ src/main/java/br/rs/matheuspadilha/core/BaseTest.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class BaseTest {
     
@@ -19,7 +21,9 @@ public class BaseTest {
     public void finaliza() throws IOException {
         TakesScreenshot ss = (TakesScreenshot) getDriver();
         File arquivo = ss.getScreenshotAs(OutputType.FILE);
-        FileUtils.copyFile(arquivo, new File("target" + File.separator + "screenshot" + File.separator + testName.getMethodName() + ".jpg"));
+        SimpleDateFormat data = new SimpleDateFormat("dd-MM-yyyy-HH-mm-ss");
+        
+        FileUtils.copyFile(arquivo, new File("target" + File.separator + "screenshot" + File.separator + testName.getMethodName() + data.format(new Date()) + ".jpg"));
         
         if (Propriedades.FECHAR_BROWSER){
             killDriver();
